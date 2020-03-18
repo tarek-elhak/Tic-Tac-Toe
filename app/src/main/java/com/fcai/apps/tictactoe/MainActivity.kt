9a7bd1fcb5f61c0b9db468d7deb743d2114d3572
tarek abdelhak mohamed <tarekroja123@gmail.com>
2020-03-18 05:40:45 +0200

    layout_added

---
+++ app/src/main/java/com/fcai/apps/tictactoe/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.fcai.apps.tictactoe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    
+}
